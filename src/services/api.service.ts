5ed4ef85b239d1e01e707c4ec27e8d6b764e6823
letniki <nikitaletuta12@gmai.com>
2024-08-08 17:32:20 +0300

    small change

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -5,18 +5,15 @@ import {IComment} from "@/models/IComment";
 const baseUrl='https://jsonplaceholder.typicode.com'
 
 const getAllUsers=async():Promise<IUser[]>=>{
-    let response=await fetch(`${baseUrl}/users`)
+    return await fetch(`${baseUrl}/users`)
         .then(value => value.json());
-    return response;
 }
 const getAllPosts=async():Promise<IPost[]>=>{
-   let response= await fetch(`${baseUrl}/posts`)
+   return await fetch(`${baseUrl}/posts`)
         .then(value => value.json());
-   return response;
 }
 const getAllComments=async():Promise<IComment[]>=>{
-    let response = fetch(`${baseUrl}/comments`)
+    return fetch(`${baseUrl}/comments`)
         .then(value => value.json());
-    return response;
 }
 export {getAllUsers, getAllPosts, getAllComments}
